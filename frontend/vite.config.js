36932c66e79fb633b3e59c3592e56e98afcb00fc
miranhusain <160420429+miranhusain@users.noreply.github.com>
2026-04-20 02:03:42 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': { target: 'http://localhost:8080', changeOrigin: true }
-    }
-  },
-  build: { outDir: '../backend/public' }
+  build: {
+    outDir: 'dist'
+  }
 });
